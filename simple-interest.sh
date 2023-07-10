b9b59e92e580e9a112b5f4c896fd5be5211753e7
Tusshar10 <131467401+Tusshar10@users.noreply.github.com>
2023-07-10 16:43:47 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # Tusshar 10
+   # Tusshar10
    # Input:
    # p, principal amount
    # t, time period in years
